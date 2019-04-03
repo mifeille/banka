4f198a3f218cff242c8b5405593c49be1ab686a4
mifeille <nimilleer@gmail.com>
2019-04-04 01:06:30 +0200

    feature:build an endpoint to update a bank account [Finishes #165105461]

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,6 +1,9 @@
 import {accountData} from '../models/accountsData';
+import {userData} from '../models/usersData'
+import validation from '../helpers/accountValidation'
 
 const allAccounts = accountData;
+const allUsers=userData;
 
 export default class authUsers{
    static getAllAccounts(req, res){
@@ -12,26 +15,58 @@ export default class authUsers{
 
     static createAccount(req, res){
         try{
-            const account = {
-                accountNumber:allAccounts.length +1,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                type: req.body.type,
-                Status:"draft",
-                openingBalance:0
+            if(validation.validateAccount(req, res)){
+                const users = allUsers.filter(user => user.email == req.body.email);
+                if(users.length < 1){
+                   return res.status(404).json({
+                        status:404,
+                        message: "Email not found"
+                    });
+                } else{
+                    const account = {
+                        accountNumber:allAccounts.length +1,
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        type: req.body.type,
+                        Status:"draft",
+                        openingBalance:0
+                    }
+                    allAccounts.push(account);
+                    let accountNumber=account.accountNumber,firstName=account.firstName,lastName=account.lastName,email=account.email,type=account.type,status=account.status,openingBalance=account.openingBalance;
+                    res.status(201).json({
+                        status :201,
+                        data: {accountNumber,firstName,lastName,email,type,status,openingBalance}
+                    });
+                }
             }
-            allAccounts.push(account);
-            let accountNumber=account.accountNumber,firstName=account.firstName,lastName=account.lastName,email=account.email,type=account.type,status=account.status,openingBalance=account.openingBalance;
-            res.status(201).json({
-                status :201,
-                data: {accountNumber,firstName,lastName,email,type,status,openingBalance}
-            });
         }
         catch(err){
             return res.status(400).json({
                 status:400,
                 message: err.message
+            });
+        }
+    }
+
+    static updateAccount(req, res){
+        try{
+            const accountNumber=req.params.accountNumber;
+            const accounts = allAccounts.filter(account => account.accountNumber == accountNumber);
+            console.log(accounts);
+            if(accounts.length==1){
+                accounts[0].status=req.body.status;
+            }
+            let accountNumb=accounts[0].accountNumber,firstName=accounts[0].firstName,lastName=accounts[0].lastName,email=accounts[0].email,type=accounts[0].type,status=accounts[0].status,openingBalance=accounts[0].openingBalance;
+            res.status(200).json({
+                status :201,
+                data: {accountNumb,firstName,lastName,email,type,status,openingBalance}
+            });
+        }
+        catch(err){
+            return res.status(404).json({
+                status:404,
+                message: "Bank account not found"
             });
         }
     }
