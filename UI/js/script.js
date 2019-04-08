81c0d87305f3cfa608079766d71f72ab7fb82e91
mifeille <nimilleer@gmail.com>
2019-04-08 20:12:04 +0200

    chore: restructuring UI files [Finishes #165174999]

---
+++ UI/js/script.js
@@ -0,0 +1,28 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); 
+}
+
+// document.getElementsByClassName("icon").onclick="myFunction()";
+
+function myFunction() {
+  var i;
+  var x = document.getElementsByClassName("myMenu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  // setTimeout(carousel, 3000); 
+  }
