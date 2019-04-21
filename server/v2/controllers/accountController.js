b47ae22bfae819b88d0c3d50ee3de47ab07ca964
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-21 12:40:08 +0200

    account activation and deactivation[Finishes #165484235]

--- server/v2/controllers/accountController.js
+++ server/v2/controllers/accountController.js
@@ -45,7 +45,7 @@ const accounts = {
                 const values = [account.accountnumber,account.createdon,account.owner,account.type,account.status,account.openingbalance,account.balance];
                 const result = await db.query(query, values);
                 let accountnumber = account.accountnumber,firstName = rows[0].firstname,lastName = rows[0].lastname,email = account.email,type = account.type,status = account.status,openingBalance = account.openingbalance;
-                res.status(201).json({
+                return res.status(201).json({
                     status :201,
                     message:"Bank account created successfully",
                     data: {accountnumber,firstName,lastName,email,type,status,openingBalance}
@@ -57,7 +57,63 @@ const accounts = {
                 message: err.message
             });
         }
+    },
+
+    async updateAccount(req, res){
+        let decodedEmail;
+            jwt.verify(req.token,process.env.JWTSECRETKEY,(err,decoded)=>{
+                if(err){
+                    return res.status(403).json({
+                        status:403,
+                        error:"A token must be provided!"
+                    });
+                }
+                decodedEmail = decoded.email; 
+            });
+
+            const admin = 'SELECT * FROM staff WHERE email = $1';
+            const findAdmin = await db.query(admin, [decodedEmail]);
+            if(findAdmin.rows == 0) {
+                return res.status(400).json({
+                    status:400,
+                    message: "Only an admin can activate or deactivate a Bank account"
+                }); 
+            }   
+            if(findAdmin.rows[0].isadmin == 'false') {
+                return res.status(400).json({
+                    status:400,
+                    message: "Only an admin can activate or deactivate a Bank account"
+                }); 
+            } else {
+            const accountNumber = req.params.accountNumber;
+            const account = 'SELECT * FROM accounts WHERE accountnumber = $1';
+            const {rows} = await db.query(account, [accountNumber]);
+                
+                if(!rows) {
+                    return res.status(404).json({
+                        status:404,
+                        message: "Bank account not found"
+                    }); 
+                } else {  
+                    if(rows[0].status === req.body.status){
+                        return res.status(400).json({
+                            status :400,
+                            message: `This account is already ${req.body.status}`
+                        });
+                    } else {
+
+                        const accountUpdate = 'UPDATE accounts SET status = $1 WHERE accountnumber = $2';
+                        const result = await db.query(accountUpdate, [req.body.status, accountNumber]);
+                        let status = req.body.status;
+                        return res.status(200).json({
+                            status :200,
+                            message : "Operation Successful",
+                            data: {accountNumber,status}
+                        });
+                    } 
+                } 
+            }
+        }
     }
-}
 
 export default accounts;
