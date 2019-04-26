f1dcb0788aca02dd2d4a9a6aea145ae6d0ca1783
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-26 12:10:35 +0200

    travis test3

--- server/test/accountV2Test.js
+++ server/test/accountV2Test.js
@@ -125,8 +125,8 @@ describe('Bank account activation and deactivation', () => {
     chai.request(server)
       .post('/api/v2/auth/signin')
       .send({
-        email: 'kabehola@banka.com',
-        password: 'kabeho1!',
+        email: process.env.superUserEmail,
+        password: process.env.superUserPassword,
       })
       .end((err, res) => {
         expect(res).to.have.status(200);
