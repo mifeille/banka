518e98659c19ce8e8b153c226aa4041ee2802442
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-26 13:13:55 +0200

    challenge 2

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import swaggerUi from 'swagger-ui-express';
 import apiRoutes from './routes/myRoutes';
-import v2Routes from './routes/v2Routes';
-import swaggerDocument from './swagger.json';
+
 
 
 dotenv.config();
@@ -12,13 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use('/banka-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 app.use('/api/v1/', apiRoutes);
-app.use('/api/v2/', v2Routes);
+
 
 
 const port = process.env.PORT || 3050;
