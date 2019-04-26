b9ca0852e11dbd753050adad1225a1bbf8d49a27
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-26 19:09:40 +0200

    fixing travis

--- server/v2/db/createTables.js
+++ server/v2/db/createTables.js
@@ -1,4 +1,3 @@
-import bcrypt from 'bcrypt';
 import queryDb from './tables';
 import pool from './dbconnection';
 
@@ -8,10 +7,6 @@ const createTables = async () => {
     await pool.query(queryDb.registerAccountTable);
     await pool.query(queryDb.registerTransactionTable);
     await pool.query(queryDb.registerNotificationTable);
-    const hash = bcrypt.hashSync(process.env.superUserPassword, 10);
-    const superUser = 'INSERT INTO users (firstname,lastname,email,password,type,isadmin) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (email) DO NOTHING';
-    const value = ['Laetitia', 'Kabeho', process.env.superUserEmail, hash, 'staff', 'true'];
-    await pool.query(superUser, value);
   } catch (err) {
     console.log(err);
   }
---
+++ server/v2/db/superUser.js
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+import queryDb from './tables';
+import pool from './dbconnection';
+
+const superUser = async () => {
+  try {
+    const hash = bcrypt.hashSync(process.env.superUserPassword, 10);
+    const firstAdmin = 'INSERT INTO users (firstname,lastname,email,password,type,isadmin) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (email) DO NOTHING';
+    const value = ['Laetitia', 'Kabeho', process.env.superUserEmail, hash, 'staff', 'true'];
+    await pool.query(firstAdmin, value);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default superUser();
