f75ecf45a86696b8f1e7f4f1a6813a82c7c5fa4a
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-27 16:48:23 +0200

    travis

--- server/v2/db/superUser.js
+++ server/v2/db/superUser.js
@@ -1,5 +1,8 @@
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 import pool from './dbconnection';
+
+dotenv.config();
 
 const superUser = async () => {
   try {
@@ -7,6 +10,10 @@ const superUser = async () => {
     const firstAdmin = 'INSERT INTO users (firstname,lastname,email,password,type,isadmin) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (email) DO NOTHING';
     const value = ['Laetitia', 'Kabeho', process.env.EMAIL, hash, 'staff', 'true'];
     await pool.query(firstAdmin, value);
+    const superUs = 'SELECT * FROM users WHERE email = $1';
+    const value1 = [process.env.EMAIL];
+    const found = await pool.query(superUs, value1);
+    console.log(found.rows);
   } catch (err) {
     console.log(err);
   }
