1c890ab5332c450da13107bc5b637699f275bb0e
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-27 15:20:45 +0200

    fixing tests

--- server/test/accountV2Test.js
+++ server/test/accountV2Test.js
@@ -141,6 +141,10 @@ describe('Bank account creation', () => {
 
 describe('Bank account activation and deactivation', () => {
   before((done) => {
+    const hash = bcrypt.hashSync(process.env.PASSWORD, 10);
+    const firstAdmin = 'INSERT INTO users (firstname,lastname,email,password,type,isadmin) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (email) DO NOTHING';
+    const value = ['Laetitia', 'Kabeho', process.env.EMAIL, hash, 'staff', 'true'];
+    pool.query(firstAdmin, value);
     done();
   });
   it('It Should create a user with right signup credentials', (done) => {
