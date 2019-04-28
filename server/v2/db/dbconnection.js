e268d8c0e7b0febf848da1baff2af34a24869a1a
mifeille <emiliennebenerugok@Emiliennes-MacBook-Air.local>
2019-04-28 21:05:32 +0200

    bug:heroku

--- server/v2/db/dbconnection.js
+++ server/v2/db/dbconnection.js
@@ -3,22 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let pool = {}
+let pool = {};
 
-if (process.env.NODEENV === 'DEV') {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-}
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-if (process.env.NODEENV === 'TEST') {
-  pool = new Pool({
-    connectionString: process.env.DATABASETEST,
-  });
-}
-if (process.env.NODEENV === 'PRODUCTION') {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_HEROKU,
-  });
-}
+
 export default pool;
